0693393dcb3b0e8734d4188db8e64360ddda0bdc
sourov25tdg <sourovonfire.20@gmail.com>
2026-09-12 23:06:47 +0600

    Initialize build.gradle.kts with Java setup

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+group = "com.dksourov"
+version = "0.1.0-26.2"
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("com.google.code.gson:gson:2.13.1")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+}
